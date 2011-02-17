64a684eff0803740f3f859ed09975a8214895a73
Gabriel Hiciano <gabriel@samesub.com>
2011-02-17 10:32:11 -0400

    Set access rules for Subject Controller

--- protected/controllers/SubjectController.php
+++ protected/controllers/SubjectController.php
@@ -27,11 +27,11 @@ class SubjectController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index','view','create'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array('update'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
