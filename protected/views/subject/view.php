7a12edefb5615c2dd5c3f2b79281577cd3d477a5
Gabriel Hiciano <gabriel@samesub.com>
2011-05-06 23:17:12 -0400

    redesign the view subject view

--- protected/views/subject/view.php
+++ protected/views/subject/view.php
@@ -19,33 +19,21 @@ $this->breadcrumbs=array(
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'title'); ?>
-		<?php echo CHtml::encode($model->title); ?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'show_time'); ?>
+		<?php echo $model->country->name . " | "; ?>
 		<?php echo CHtml::encode(date("Y/m/d H:i", $model->show_time)." UTC"); ?>
 	</div>
+
 	<div class="row">
-		<?php echo $form->labelEx($model,'content_type_id'); ?>
-		<?php echo CHtml::encode($model->content_type->fullname); ?> 
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'Content'); ?>
 		<?php echo SiteHelper::subject_content($model); ?>
 	</div>
+
 	<div class="row">
-		<?php echo $form->labelEx($model,'country_id'); ?>
-		<?php echo $model->country->name; ?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model,'user_comment'); ?>
 		<?php echo SiteHelper::formatted($model->user_comment); ?>
 	</div>
 	
 
 <?php $this->endWidget(); ?>
-<h3>Comments:</h3>
+<h4>Comments:</h4>
 
 <?php foreach($model->comments as $comment): ?>
 <div class="comment" id="c<?php echo $comment->id; ?>">
